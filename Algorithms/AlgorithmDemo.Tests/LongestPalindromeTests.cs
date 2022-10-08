c0466d18ed12cf9ff69105079d3be39e90c239a0
fred <domaine75@gmail.com>
2022-10-08 21:22:47 +0200

    clean code

--- Algorithms/AlgorithmDemo.Tests/LongestPalindromeTests.cs
+++ Algorithms/AlgorithmDemo.Tests/LongestPalindromeTests.cs
@@ -3,18 +3,17 @@ using Xunit;
 
 namespace AlgorithmDemo.Tests
 {
-    public class LongestPalindromeTests
+  public class LongestPalindromeTests
+  {
+    [Theory]
+    [InlineData("babad", "bab")]
+    [InlineData("cbbd", "bb")]
+    [InlineData("ac", "a")]
+    [InlineData("a", "a")]
+    public void LongestPalindrome_ExpandWithCorners(string input, string expectedOutput)
     {
-        [Theory]
-        [InlineData("babad","bab")]
-        [InlineData("cbbd","bb")]
-        [InlineData("ac","a")]
-        [InlineData("a","a")]
-        public void LongestPalindrome_ExpandWithCorners(string input,string expectedOutput)
-        {
-            var alg = new LongestPalindrome();
-            var result = alg.ExpandingWithCorners(input);
-            Assert.Equal(expectedOutput,result);
-        }
+      var result = LongestPalindrome.ExpandingWithCorners(input);
+      Assert.Equal(expectedOutput, result);
     }
+  }
 }
--- Algorithms/AlgorithmDemo/LongestPalindrome.cs
+++ Algorithms/AlgorithmDemo/LongestPalindrome.cs
@@ -2,37 +2,37 @@
 
 namespace AlgorithmDemo
 {
-    public class LongestPalindrome
+  public class LongestPalindrome
+  {
+    public static string ExpandingWithCorners(string src)
     {
-        public string ExpandingWithCorners(string src)
+      var left = 0;
+      var max = 0;
+      for (int i = 0; i < src.Length; i++)
+      {
+        var l1 = GetLength(src, i, i);
+        var l2 = GetLength(src, i, i + 1);
+        var current = Math.Max(l1, l2);
+
+        if (current > max)
         {
-            var left = 0;
-            var max = 0;
-            for(int i=0;i<src.Length; i++)
-            {
-                var l1 = GetLength(src, i, i);
-                var l2 = GetLength(src, i, i+1);
-                var current = Math.Max(l1, l2);
-
-                if(current > max)
-                {
-                    max = current;
-                    left = i-(current-1)/2;
-                }
-            }
-
-            return src.Substring(left, max);
+          max = current;
+          left = i - (current - 1) / 2;
         }
+      }
 
-        private int GetLength(string src,int left,int right)
-        {
-            while(left>=0 && right < src.Length && src[left]==src[right])
-            {
-                left--;
-                right++;
-            }
-
-            return right - (left+1);
-        }
+      return src.Substring(left, max);
     }
+
+    private static int GetLength(string src, int left, int right)
+    {
+      while (left >= 0 && right < src.Length && src[left] == src[right])
+      {
+        left--;
+        right++;
+      }
+
+      return right - (left + 1);
+    }
+  }
 }
